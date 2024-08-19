7dfb2c541c27dd896e5d6bd44fc35551e0e0d955
Puddi <evilgerm@gmail.com>
2024-08-20 00:03:07 +0200

    no linting, only ruffing

--- rundevenv.py
+++ rundevenv.py
@@ -14,7 +14,7 @@ class Command:
 
     @classmethod
     def run(cls, command):
-        '''Takes in a class command to run it'''
+        """Takes in a class command to run it"""
         if cls.is_process_running(command):
             print(f"{command} already running passing...")
             return  # process already running, do nothing
@@ -27,15 +27,17 @@ class Command:
 
     @classmethod
     def is_process_running(cls, command):
-        '''checks if the process is already running'''
-        wslexes = ['wslservice.exe', 'wslrelay.exe', 'wslhost.exe']
+        """checks if the process is already running"""
+        wslexes = ["wslservice.exe", "wslrelay.exe", "wslhost.exe"]
         splitcommand = re.split(r'[\\ "]', command)
-        for proc in psutil.process_iter(['pid', 'name']):
+        for proc in psutil.process_iter(["pid", "name"]):
             # some system processes will not have a name
-            if (any(part in proc.info['name'] for part in splitcommand) and
-                    len(proc.info['name']) > 1
-                    and proc.info['name'] not in wslexes):
-                print(proc.info['name'])
+            if (
+                any(part in proc.info["name"] for part in splitcommand)
+                and len(proc.info["name"]) > 1
+                and proc.info["name"] not in wslexes
+            ):
+                print(proc.info["name"])
                 return True
         return False
 
@@ -44,29 +46,35 @@ class Command:
 
 
 class Browser:
-    '''Class for browser opening as chromium browser behave differently to other windows binaries'''
+    """Class for browser opening as chromium browser behave differently to other windows binaries"""
+
     @classmethod
     def open(cls, url1, url2):
-        '''opens url in default browser, takes in 2 urls'''
+        """opens url in default browser, takes in 2 urls"""
         if len(pyautogui.getWindowsWithTitle("brave")) == 1:
-            subprocess.run(r'"C:\Program Files\BraveSoftware\Brave-Browser\Application\brave.exe"'
-                           + ' ' + url1 + ' --new-window', check=False)
+            subprocess.run(
+                r'"C:\Program Files\BraveSoftware\Brave-Browser\Application\brave.exe"'
+                + " "
+                + url1
+                + " --new-window",
+                check=False,
+            )
             webbrowser.open(url2)
         else:
-            print('Multiple brave.exe processes found passing...')
+            print("Multiple brave.exe processes found passing...")
         sleep(2)
 
 
 try:
     if VirtualDesktop.current().number == 1:
-        print('Switching Vdesktop')
+        print("Switching Vdesktop")
         VirtualDesktop(2).go()
         sleep(1)
-        print('Launching Pycharm')
+        print("Launching Pycharm")
         Command.run(Command.pycharm)
-        print('Launching wsl')
+        print("Launching wsl")
         Command.run(Command.wsl)
-        print('Launching jupyter and portainer')
+        print("Launching jupyter and portainer")
         Browser.open("https://portainer.local:9443", "http://127.0.0.1:666/lab")
 except Exception as e:
     print(e)
